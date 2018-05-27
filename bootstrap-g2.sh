d7f5575e11bac371bc1370050d2d54f44a417ad3
Samriddha Chatterjee <Samriddha.Chatterjee@hcl.com>
2018-05-27 22:03:30 +0530

    Dev

--- bootstrap-g2.sh
+++ bootstrap-g2.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
-
+sudo apt-get update -y
 #nvidia installation.
 sudo apt-get install openjdk-8-jdk git python-dev python3-dev python-numpy python3-numpy build-essential python-pip python3-pip python-virtualenv swig python-wheel libcurl3-dev curl -y
 curl -O https://s3-us-west-2.amazonaws.com/sam-tensorflow-installers/cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
 sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
 sudo dpkg -i ./cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
+sudo apt-get update -y
 sudo apt-get install cuda-9-0 -y
 wget https://s3-us-west-2.amazonaws.com/sam-tensorflow-installers/cudnn-9.0-linux-x64-v7.1.tgz
 sudo tar -xzvf cudnn-9.0-linux-x64-v7.1.tgz
 sudo cp cuda/include/cudnn.h /usr/local/cuda/include
 sudo cp cuda/lib64/libcudnn* /usr/local/cuda/lib64
 sudo chmod a+r /usr/local/cuda/include/cudnn.h /usr/local/cuda/lib64/libcudnn*
-
+echo 'export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/cuda/lib64:/usr/local/cuda/extras/CUPTI/lib64"' >> ~/.bashrc
+echo 'export CUDA_HOME=/usr/local/cuda' >> ~/.bashrc
+source ~/.bashrc
 
 #python
 wget https://repo.continuum.io/archive/Anaconda3-5.1.0-Linux-x86_64.sh
