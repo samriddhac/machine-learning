d48c2f2e39f5982f8771c22a0f23a403c524cac1
Samriddha Chatterjee <Samriddha.Chatterjee@hcl.com>
2018-05-29 13:16:54 +0530

    Dev

--- bootstrap-g2.sh
+++ bootstrap-g2.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd /home/ubuntu
 sudo apt-get update -y
 #nvidia installation.
 sudo apt-get install openjdk-8-jdk git python-dev python3-dev python-numpy python3-numpy build-essential python-pip python3-pip python-virtualenv swig python-wheel libcurl3-dev curl -y
@@ -12,9 +13,6 @@ sudo tar -xzvf cudnn-9.0-linux-x64-v7.1.tgz
 sudo cp cuda/include/cudnn.h /usr/local/cuda/include
 sudo cp cuda/lib64/libcudnn* /usr/local/cuda/lib64
 sudo chmod a+r /usr/local/cuda/include/cudnn.h /usr/local/cuda/lib64/libcudnn*
-echo 'export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/cuda/lib64:/usr/local/cuda/extras/CUPTI/lib64"' >> ~/.bashrc
-echo 'export CUDA_HOME=/usr/local/cuda' >> ~/.bashrc
-source ~/.bashrc
 
 #python
 wget https://repo.continuum.io/archive/Anaconda3-5.1.0-Linux-x86_64.sh
